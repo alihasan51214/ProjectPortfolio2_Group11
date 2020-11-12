4f21cee398750a2cba1089ead8247741659611a3
bobxwins <37103072+bobxwins@users.noreply.github.com>
2020-11-12 12:35:01 +0100

    DTOS created

--- DataServiceLib/DataService.cs
+++ DataServiceLib/DataService.cs
@@ -9,12 +9,7 @@ namespace DataServiceLib
   public class DataService : IDataService  //IDataService is an interface used by the DataService. 
         //All the Create/Get/Update/Delete functions used in this class are made in the interface
     {    
-        private readonly string _connectionString;
-
-        public DataService(string connectionString) 
-        {
-            _connectionString = connectionString; 
-        }
+      
 
  
         public List<BookmarkPerson> _BookmarkPerson = IDataService.BookmarkPerson;
@@ -146,110 +141,6 @@ namespace DataServiceLib
         {
             return _users.FirstOrDefault(x => x.UserId == userid);
         }
-
-
-        /*
-        public IList<ActorsKnownForTitles> GetActorsKnownForTitles()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.ActorsKnownForTitles.ToList();
-        }
-
-        public IList<ActorsProfession> GetActorsProfession()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.ActorsProfessions.ToList();
-        }
-        
-        public IList<BookmarkPerson> GetBookmarkPerson()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.BookmarkPerson.ToList();
-        }
-
-        public IList<Bookmarktitle> GetBookmarktitle()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.Bookmarktitle.ToList();
-        }
-        
-        public IList<Directors> GetDirectors()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.Directors.ToList();
-        }
-
-        public IList<NameBasics> GetNameBasics()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.NameBasics.ToList();
-        }
-
-        public IList<SearchHistory> GetSearchHistory()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.SearchHistory.ToList();
-        }
-
-        public IList<TitleAkas> GetTitleAkas()
-        {
-            using var ctx = new Raw11Context(_connectionString); 
-            return ctx.TitleAkas.ToList();
-        }
-        
-        public IList<TitleBasics> GetTitleBasics()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.TitleBasics.ToList();
-        }
-        
-        public IList<TitlePrincipals> TitlePrincipals()
-        {
-            using var ctx = new Raw11Context(_connectionString); 
-            return ctx.TitlePrincipals.ToList();
-        }
-
-        public IList<TitleEpisode> TitleEpisode()
-        {
-            using var ctx = new Raw11Context(_connectionString); 
-            return ctx.EpisodeTitles.ToList();
-        }
-        
-        public IList<TitleGenres> Genres()
-        {
-            using var ctx = new Raw11Context(_connectionString); 
-            return ctx.Genres.ToList();
-        }
-        
-        public IList<UserNameRate> GetUserNameRate()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.UserNameRates.ToList();
-        }
-
-        public IList<UserTitleRate> GetUserTitleRate()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.UserTitleRates.ToList();
-        }
-        
-        public IList<Users> GetUsers()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.Users.ToList();
-        }
-
-        public IList<WordSearch> GetWordSearch()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.WordSearch.ToList();
-        }
-
-        public IList<Writer> GetWriter()
-        {
-            using var ctx = new Raw11Context(_connectionString);
-            return ctx.Writers.ToList();
-        }
-        */
+         
     }
 }
---
+++ WebService/Model/UserNameRateForCreationOrUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataServiceLib.DBObjects
+{
+    public class UserNameRateForCreationOrUpdateDTO
+    { 
+      
+        public int NameIndividRating { get; set; }
+        public string Nconst { get; set; }
+        public DateTime DateTime { get; set; }
+    
+    }
+}
