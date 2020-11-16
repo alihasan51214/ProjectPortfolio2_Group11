00ca922cd8f5862aba602cfaa59951048ec9e63e
bobxwins <37103072+bobxwins@users.noreply.github.com>
2020-11-16 19:25:52 +0100

    minor edit requests

--- WebService/Controller/BookmarkController.cs
+++ WebService/Controller/BookmarkController.cs
@@ -40,10 +40,11 @@ namespace ProjectPortfolio2_Group11.Controller
         [HttpGet("{userId}/{nConst}")]
         public IActionResult GetBookmark(int userId,string nConst)
         {
+            var response = " bookmark not found";
             var bookmark = _dataServiceFacade.BookmarkingDs.GetBookMark(userId,nConst);
             if (bookmark == null)
             {
-                return NotFound();
+                return NotFound(response);
             }
             return Ok(_mapper.Map<BookmarkPersonDto>(bookmark));
         }
@@ -51,10 +52,11 @@ namespace ProjectPortfolio2_Group11.Controller
         [HttpPost("{userId}")]
         public IActionResult UpdateBookmark(BookmarkPersonForCreationDto bookmarkPersonForCreationDtoDto)
         {
+            var response = " This bookmark already exists for this user";
             var bookmark = _mapper.Map<BookmarkPerson>(bookmarkPersonForCreationDtoDto);
             if (!_dataServiceFacade.BookmarkingDs.CreateBookmark(bookmark))
             {
-                return NotFound();
+                return BadRequest(response);
             }
             return Created("", bookmark);
         }
